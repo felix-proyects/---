8142778998cc29e5f85e005a53d86ed0bae1e50d
FELIX-OFC <felixofc3@gmail.com>
2025-08-24 02:01:13 -0400

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -3,10 +3,13 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn }) => {
 
   const namegrupo = 'Grupo Oficial'
-  const gp1 = 'https://chat.whatsapp.com/KoJjHo6o3Ew7P5qkjaIh0r' // ← tu link real
+  const gp1 = 'https://chat.whatsapp.com/ETZduk7trjG9xgTXVCRHYK?mode=ems_copy_c' // ← tu link real
 
   const namechannel = 'Canal de la Bot'
-  const channel = 'https://whatsapp.com/channel/0029VbAZcyIIXnlwp79iwu2l' // ← tu canal real
+  const channel = 'https://whatsapp.com/channel/0029Vb5nxWWFHWq5CNFP5b21' // ← tu canal real
+
+  const namehosting = 'Canal del host'
+  const channelhosting = 'https://whatsapp.com/channel/0029VbAa5sNCsU9Hlzsn651S' // ← tu canal real
 
   const dev = '💎 Creador: Félix Manuel'
   const catalogo = 'https://qu.ax/dXOUo.jpg' // o './media/grupos.jpg'
@@ -20,6 +23,9 @@ let handler = async (m, { conn }) => {
 │
 │ 🩵 *${namechannel}*
 │ ${channel}
+│
+│ 💙  *${namehosting}*
+│ ${channelhosting}   
 ╰─────────────────╯
 `
 
