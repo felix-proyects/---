58a941debf5276f540e5bf4f7bc902af95d258db
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2025-08-24 20:40:34 -0600

    Update owner-tokens.js

--- plugins/owner-tokens.js
+++ plugins/owner-tokens.js
@@ -99,7 +99,7 @@ const handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
                 token: tokenString,
                 estado: "libre",
                 numero: null,
-                premium: false
+                premium: true
             };
 
             tokens.push(newToken);
@@ -138,7 +138,7 @@ const handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
             token.token = newToken;
             token.estado = "libre";
             token.numero = null;
-            token.premium = false;
+            token.premium = true;
 
             fs.writeFileSync(tokensFilePath, JSON.stringify(tokens, null, 2));
 
