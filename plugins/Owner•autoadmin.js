f752ff836fecba91c31cd24b6181e51dbac4a82c
felix-proyects <frasesbebor@gmail.com>
2025-08-26 18:29:58 -0400

    Actualizar Owner•autoadmin.js

--- plugins/Owner•autoadmin.js
+++ plugins/Owner•autoadmin.js
@@ -1,13 +1,13 @@
 const handler = async (m, {conn, isAdmin, groupMetadata }) => {
-  if (isAdmin) return m.reply('💎 *Ahora eres admin.*');
+  if (isAdmin) return m.reply('☆ *Ahora eres admin.*');
   try {
     await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
   await m.react(done)
-   m.reply('✧ *Ya te di admin.*');
+   m.reply('☆ *Ya te di admin.*');
     let nn = conn.getName(m.sender);
-     conn.reply('18293142989@s.whatsapp.net', `🩵 *${nn}* se dio Auto Admin en:\n${groupMetadata.subject}.`, m, fake, );
+     conn.reply('18293142989@s.whatsapp.net', `☆ *${nn}* se dio Auto Admin en:\n${groupMetadata.subject}.`, m, fake, );
   } catch {
-    m.reply('💎 Ocurrio un error.');
+    m.reply('☆ Ocurrio un error.');
   }
 };
 handler.tags = ['owner'];
