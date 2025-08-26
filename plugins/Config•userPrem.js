b79f16f02931e8c384af8d3889cdc71cfc7e28b4
felix-proyects <frasesbebor@gmail.com>
2025-08-26 17:25:15 -0400

    Actualizar Config•userPrem.js

--- plugins/Config•userPrem.js
+++ plugins/Config•userPrem.js
@@ -8,7 +8,7 @@ export async function all(m) {
         user.premium = false;
         const JID = Object.keys(global.db.data.users).find((key) => global.db.data.users[key] === user);
         const usuarioJid = JID.split`@`[0];
-        const textoo = `「🩵」Se acabo tu tiempo como usuario premium`;
+        const textoo = `☆ Se acabo tu tiempo como usuario premium`;
         await this.sendMessage(JID, {text: textoo, mentions: [JID]}, {quoted: fkontak });
       }
     }
