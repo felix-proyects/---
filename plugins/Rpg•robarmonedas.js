cf7ea0de1ef35e049d3523474469ea3c08a4e494
felix-proyects <frasesbebor@gmail.com>
2025-08-26 18:45:00 -0400

    Actualizar Rpg•robarmonedas.js

--- plugins/Rpg•robarmonedas.js
+++ plugins/Rpg•robarmonedas.js
@@ -2,26 +2,26 @@ const ro = 30;
 const handler = async (m, {conn, usedPrefix, command}) => {
   const time = global.db.data.users[m.sender].lastrob2 + 7200000;
   if (new Date - global.db.data.users[m.sender].lastrob2 < 7200000) {
-  conn.reply(m.chat, `*🩵 Hey! Espera ${msToTime(time - new Date())} para volver a robar*`, m, fake);
+  conn.reply(m.chat, `*☆ Hey! Espera ${msToTime(time - new Date())} para volver a robar*`, m, fake);
   return;
   }
   let who;
   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
   else who = m.chat;
   if (!who) {
-  conn.reply(m.chat, `*🩵 Etiqueta al usuario.*`, m, fake)
+  conn.reply(m.chat, `*☆ Etiqueta al usuario.*`, m, fake)
   return;
     };
   if (!(who in global.db.data.users)) { 
-  conn.reply(m.chat, `*🩵 El usuario no se encuentra en mi base de datos.*`, m, fake)
+  conn.reply(m.chat, `*☆ El usuario no se encuentra en mi base de datos.*`, m, fake)
 return;
   }
   const users = global.db.data.users[who];
   const rob = Math.floor(Math.random() * ro);
-  if (users.cookies < rob) return conn.reply(m.chat, `😔 @${who.split`@`[0]} Tiene menos de *${ro} Diamantes*\nNo robes a un pobre :v`, m, {mentions: [who]});
+  if (users.cookies < rob) return conn.reply(m.chat, `☆ @${who.split`@`[0]} Tiene menos de *${ro} Diamantes*\nNo robes a un pobre :v`, m, {mentions: [who]});
   global.db.data.users[m.sender].cookies += rob;
   global.db.data.users[who].cookies -= rob;
-  conn.reply(m.chat, `*🩵 Robastes ${rob} Diamantes a @${who.split`@`[0]}*`, m, {mentions: [who]});
+  conn.reply(m.chat, `*☆ Robastes ${rob} Diamantes a @${who.split`@`[0]}*`, m, {mentions: [who]});
   global.db.data.users[m.sender].lastrob2 = new Date * 1;
 };
 handler.help = ['rob2'];
