cf8fc62baef94d823a8295c56efe43cd24bf6edd
felix-proyects <frasesbebor@gmail.com>
2025-08-27 15:20:26 -0400

    Actualizar Grupo•tagall.js

--- plugins/Grupo•tagall.js
+++ plugins/Grupo•tagall.js
@@ -1,39 +1,17 @@
-//* Código Creado por Félix*
-//*No quites Los Créditos*
-
-
-const handler = async (m, { isOwner, isAdmin, conn, participants, args, usedPrefix }) => {
-  if (usedPrefix == 'a' || usedPrefix == 'A') return;
-  if (!(isAdmin || isOwner)) {
-    global.dfail('admin', m, conn);
-    return;
+let handler = async (m, { conn, command }) => {
+  // Reacciona con ❌
+  if (conn.sendMessage) {
+    await conn.sendMessage(m.chat, { react: { text: '❌', key: m.key } });
   }
 
-  const pesan = args.join(' ');
-  const invocador = m.pushName || 'Administrador';
-  const pp = 'https://qu.ax/MEtgi.jpg'; // Imagen para el comando 
+  // Envía el mensaje personalizado
+  await conn.sendMessage(m.chat, { 
+    text: `❏ El comando *${command}* no esta disponible para ti.\n\n> ✐ Usa #help para ver los comandos disponibles.`
+  }, { quoted: m });
+}
 
-  let teks = `╭─╮︹︹⊹︹︹⊹︹︹⊹︹︹╭─╮
-  𝗜𝗡𝗩𝗢𝗖𝗔𝗡𝗗𝗢 𝗚𝗥𝗨𝗣𝗢
-╚▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬▭╝
+handler.help = ['owner']
+handler.tags = ['main']
+handler.command = ['tagall']
 
-🌪 Te invocó: ${invocador}
-
-🌪  Mensaje: ${pesan ? pesan : ''}
-
-╭─⬣「 ✰𝗠𝗶𝗲𝗺𝗯𝗿𝗼𝘀✰ 」⬣\n`;
-  for (const mem of participants) {
-    teks += `│⁖ฺ۟̇࣪·֗٬̤⃟🌫 @${mem.id.split('@')[0]}\n`;
-  }
-  teks += '╰─⬣';
-
-  // Enviar el mensaje como imagen + texto, mencionando a todos
-  await conn.sendFile(m.chat, pp, 'invocando.jpg', teks, m, false, { mentions: participants.map(a => a.id) });
-};
-
-handler.help = ['tagall *<mensaje>*', 'invocar *<mensaje>*'];
-handler.tags = ['grupo'];
-handler.command = ['tagall', 'todos', 'mensionall', 'invocar'];
-handler.admin = true;
-handler.group = true;
-export default handler;
+export default handler
