54da9555a326da0f2c2c99553ade9e89f5a6d287
felix-proyects <frasesbebor@gmail.com>
2025-08-27 14:30:26 -0400

    Actualizar Config•autodetect.js

--- plugins/Config•autodetect.js
+++ plugins/Config•autodetect.js
@@ -14,8 +14,8 @@ foto = `《✦》Se ha cambiado la imagen del grupo.\n\n> ✧ Acción hecha por:
 edit = `《✦》${usuario} Ha permitido que ${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} puedan configurar el grupo.`
 newlink = `《✦》El enlace del grupo ha sido restablecido.\n\n> ✧ Acción hecha por:\n> » ${usuario}`
 status = `《✦》El grupo ha sido ${m.messageStubParameters[0] == 'on' ? '*cerrado*' : '*abierto*'} Por ${usuario}\n\n> ✧ Ahora ${m.messageStubParameters[0] == 'on' ? '*solo admins*' : '*todos*'} pueden enviar mensaje.`
-admingp = `「🌪」 NUEVO ADMIN EN EL GRUPO.\n\nNombre: @${m.messageStubParameters[0].split`@`[0]}\n\nLe puso admin: ${usuario}\n\ndeymoon-club.vercel.app/`
-noadmingp =  `「🌪」UN ADMIN MENOS EN EL GRUPO.\n\nNombre: @${m.messageStubParameters[0].split`@`[0]}\n\nLe quito admin: ${usuario}\ndeymoon-club.vercel.app/`
+admingp = `> ✐ Aviso: @${m.messageStubParameters[0].split`@`[0]} fue añadido a Administrador del grupo por ${usuario}.`
+noadmingp =  `> ✐ Aviso: @${m.messageStubParameters[0].split`@`[0]} fue degradado de Administrador del grupo por ${usuario}.`
 aceptar = `✦ Ha llegado un nuevo participante al grupo.\n\n> ◦ ✐ Grupo: *${groupMetadata.subject}*\n\n> ◦ ⚘ Bienvenido/a: @${m.messageStubParameters[0].split('@')[0]}\n\n> ◦ ✧ Aceptado por: @${m.sender.split('@')[0]}` 
 
 if (chat.detect && m.messageStubType == 21) {
