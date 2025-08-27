00e5c51c587933a02afdfd913cecbc6c1c3ab94a
felix-proyects <frasesbebor@gmail.com>
2025-08-26 21:20:50 -0400

    Actualizar jadibot-serbot-info.js

--- plugins/jadibot-serbot-info.js
+++ plugins/jadibot-serbot-info.js
@@ -98,7 +98,7 @@ let handler = async (m, { conn: _envio, command, usedPrefix, args, text, isOwner
 
 ${detallesBots}
 
-> Deymoon ofc`.trim();
+> ${dev}`.trim();
 
         await _envio.sendMessage(m.chat, {
           text: textoFinal,
