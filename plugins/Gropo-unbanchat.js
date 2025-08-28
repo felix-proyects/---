f9b8c428e75d378dea5fc401e9a6153e370c596d
felix-proyects <frasesbebor@gmail.com>
2025-08-28 15:19:03 -0400

    Actualizar Gropo-unbanchat.js

--- plugins/Gropo-unbanchat.js
+++ plugins/Gropo-unbanchat.js
@@ -1,11 +1,22 @@
-let handler = async (m, { conn, isAdmin, isROwner} ) => {
-    if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
-    global.db.data.chats[m.chat].isBanned = false
-    await conn.reply(m.chat, '☆ El bot ya podra responder aquí.', m, fake)
-    await m.react('✅')
+import fs from 'fs'
+import path from 'path'
+
+let handler = async (m, { conn, isAdmin, isROwner }) => {
+  const senderNumber = m.sender.replace(/[^0-9]/g, '')
+  const botPath = path.join('./JadiBots', senderNumber)
+
+
+  if (!(isAdmin || isROwner || fs.existsSync(botPath))) {
+    return m.reply('☆ No tienes permisos para usar este comando. Solo admins, owner o subbots pueden hacerlo.')
+  }
+
+  global.db.data.chats[m.chat].isBanned = false
+  m.reply('🧃☆Bot desbaneado en este grupo.')
 }
+
 handler.help = ['desbanearbot']
 handler.tags = ['group']
 handler.command = ['desbanearbot', 'unbanchat']
 handler.group = true 
+
 export default handler
