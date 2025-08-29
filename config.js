7759c212e4eca220fe0a16f5ce02c325b937f354
felix-proyects <frasesbebor@gmail.com>
2025-08-29 09:49:43 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ global.makiJadibts = true
 
 global.packsticker = `⏤͟͞ू⃪  ̸̷͢𝐃𝐞𝐲𝐦𝐨𝐨𝐧 𝐔𝐥𝐭𝐫𝐚𑁯ᰍ\n`
 global.packname = `=͟͟͞𝐃𝐞𝐲𝐦𝐨𝐨𝐧 𝐔𝐥𝐭𝐫𝐚 𝐁𝐨𝐭⏤͟͟͞͞★`
-global.author = `socket:\n✐ =͟͟͞𝐃𝐞𝐲𝐦𝐨𝐨𝐧 𝐔𝐥𝐭𝐫𝐚 𝐁𝐨𝐭⏤͟͟͞͞★\n👹 Usuario:\n➪ --`;
+global.author = `socket:\n✐ =͟͟͞𝐃𝐞𝐲𝐦𝐨𝐨𝐧 𝐔𝐥𝐭𝐫𝐚 𝐁𝐨𝐭⏤͟͟͞͞★\n👹 Usuario:\n➪ ${senderName}`;
 global.wm = '⍴᥆ᥕᥱrᥱძ ᑲᥡ ძᥱᥡm᥆᥆ᥒ ᥆𝖿ᥴ';
 global.titulowm = '⏤͟͞ू⃪𝐃𝐞𝐲𝐦𝐨𝐨𝐧 𝐔𝐥𝐭𝐫𝐚 𝐁𝐨𝐭𑁯ᰍ';
 global.igfg = 'Deymoon Club'
