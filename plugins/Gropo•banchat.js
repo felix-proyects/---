8850a1401263dddbaed80b9f3c41d534531c5e7e
felix-proyects <frasesbebor@gmail.com>
2025-08-29 02:09:59 -0400

    Actualizar Gropo•banchat.js

--- plugins/Gropo•banchat.js
+++ plugins/Gropo•banchat.js
@@ -1,13 +1,17 @@
-const senderNumber = m.sender.replace(/[^0-9]/g, '')
+import fs from 'fs'
+import path from 'path'
+
+let handler = async (m, { conn, isAdmin, isROwner }) => {
+  const senderNumber = m.sender.replace(/[^0-9]/g, '')
   const botPath = path.join('./JadiBots', senderNumber)
 
 
   if (!(isAdmin || isROwner || fs.existsSync(botPath))) {
-    return m.reply('☆ No tienes permisos para usar este comando. Solo admins o el bot pueden hacerlo.')
+    return m.reply('☆ No tienes permisos para usar este comando. Solo admins, o el bot pueden hacerlo.')
   }
 
   global.db.data.chats[m.chat].isBanned = true
-  m.reply('☆ Bot baneado para este grupo.')
+  m.reply('☆ Bot baneado con éxito.')
 }
 
 handler.help = ['banearbot']
