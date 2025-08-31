bb0815352e19bd553dddb5111907cfd214a87630
felix-proyects <frasesbebor@gmail.com>
2025-08-31 10:58:38 -0400

    Actualizar gc-kick.js

--- plugins/gc-kick.js
+++ plugins/gc-kick.js
@@ -11,20 +11,20 @@ var handler = async (m, { conn, participants, usedPrefix, command }) => {
     //const nn = conn.getName(m.sender);
 
     if (user === conn.user.jid) {
-        return conn.reply(m.chat, '🩵 No puedo eliminar el bot del grupo', m, fake, );
+        return conn.reply(m.chat, '☆ No puedo eliminar el bot del grupo', m, fake, );
     }
 
     if (user === ownerGroup) {
-        return conn.reply(m.chat, '🩵 No puedo eliminar al propietario del grupo', m, fake, );
+        return conn.reply(m.chat, '☆ No puedo eliminar al propietario del grupo', m, fake, );
     }
 
     if (user === ownerBot) {
-        return conn.reply(m.chat, '🩵 No puedo eliminar al propietario del bot', m, fake );
+        return conn.reply(m.chat, '☆ No puedo eliminar al propietario del bot', m, fake );
     }
 
     await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
 
-//conn.reply('525544876071@s.whatsapp.net', `🩵 Un Admin Acabo De Eliminar Un Usuario En El Grupo:\n> ${groupMetadata.subject}.`, m, fake, );
+//conn.reply('18293142989@s.whatsapp.net', `☆ Un Admin Acabo De Eliminar Un Usuario En El Grupo:\n> ${groupMetadata.subject}.`, m, fake, );
 };
 
 handler.help = ['kick'];
