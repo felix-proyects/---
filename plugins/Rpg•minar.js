35a97e60252b23941fc4d66a6b815b05d8129b29
felix-proyects <frasesbebor@gmail.com>
2025-09-03 17:45:39 -0400

    Actualizar Rpg•minar.js

--- plugins/Rpg•minar.js
+++ plugins/Rpg•minar.js
@@ -27,8 +27,8 @@ let handler = async (m, { conn }) => {
 
   // ENVÍO DEL MENSAJE COMO REENVIADO DESDE CANAL/NEWSLETTER
   const channelRD = { 
-    id: "120363418804796632@newsletter", // <-- Pon aquí el ID de tu canal/newsletter
-    name: "DeymoonClub"              // <-- Pon aquí el nombre del canal/newsletter
+    id: "12036341880wsletter", // <-- Pon aquí el ID de tu canal/newsletter
+    name: "offf"              // <-- Pon aquí el nombre del canal/newsletter
   }
 
   await conn.sendMessage(m.chat, {
