da5ccf4bc8eb94006ad48cf22f42d5dd9b5598a6
felix-proyects <frasesbebor@gmail.com>
2025-09-05 09:25:02 -0400

    Actualizar gacha-rollwaifu.js

--- plugins/gacha-rollwaifu.js
+++ plugins/gacha-rollwaifu.js
@@ -10,7 +10,7 @@ async function loadCharacters() {
         const data = await fs.readFile(charactersFilePath, 'utf-8')
         return JSON.parse(data)
     } catch (error) {
-        throw new Error('❀ No se pudo cargar el archivo characters.json.')
+        throw new Error('No se pudo cargar el archivo characters.json.')
     }
 }
 
@@ -18,7 +18,7 @@ async function saveCharacters(characters) {
     try {
         await fs.writeFile(charactersFilePath, JSON.stringify(characters, null, 2), 'utf-8')
     } catch (error) {
-        throw new Error('❀ No se pudo guardar el archivo characters.json.')
+        throw new Error('No se pudo guardar el archivo characters.json.')
     }
 }
 
@@ -35,7 +35,7 @@ async function saveHarem(harem) {
     try {
         await fs.writeFile(haremFilePath, JSON.stringify(harem, null, 2), 'utf-8')
     } catch (error) {
-        throw new Error('❀ No se pudo guardar el archivo harem.json.')
+        throw new Error('No se pudo guardar el archivo harem.json.')
     }
 }
 
@@ -47,7 +47,7 @@ let handler = async (m, { conn }) => {
         const remainingTime = Math.ceil((cooldowns[userId] - now) / 1000)
         const minutes = Math.floor(remainingTime / 60)
         const seconds = remainingTime % 60
-        return await conn.reply(m.chat, `《✧》Debes esperar *${minutes} minutos y ${seconds} segundos* para usar *#rw* de nuevo.`, m)
+        return await conn.reply(m.chat, `☆ Espera *${minutes} minutos y ${seconds} segundos* para usar *#rw* de nuevo.`, m, fake)
     }
 
     try {
@@ -61,12 +61,12 @@ let handler = async (m, { conn }) => {
             ? `Reclamado por @${randomCharacter.user.split('@')[0]}` 
             : 'Libre'
 
-        const message = `❀ Nombre » *${randomCharacter.name}*
-⚥ Género » *${randomCharacter.gender}*
-✰ Valor » *${randomCharacter.value}*
-♡ Estado » ${statusMessage}
+        const message = `♡ Nombre » *${randomCharacter.name}*
+☆ Género » *${randomCharacter.gender}*
+☆ Valor » *${randomCharacter.value}*
+☆ Estado » ${statusMessage}
 ❖ Fuente » *${randomCharacter.source}*
-✦ ID: *${randomCharacter.id}*`
+☆ ID: *${randomCharacter.id}*`
 
         const mentions = userEntry ? [userEntry.userId] : []
         await conn.sendFile(m.chat, randomImage, `${randomCharacter.name}.jpg`, message, m, { mentions })
