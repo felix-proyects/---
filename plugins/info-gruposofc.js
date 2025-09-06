6ae99b28842c73acd7544d5e7420a8d37ecb175e
felix-proyects <frasesbebor@gmail.com>
2025-09-06 12:51:59 -0400

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,1 +1,30 @@
-//Código en proceso 
+const handler = async (m, { conn, command }) => {
+  // Animación de "calculando..."
+  const loadingBars = [
+    "《 █▒▒▒▒▒▒▒▒▒▒▒》10%",
+    "《 ████▒▒▒▒▒▒▒▒》30%",
+    "《 ███████▒▒▒▒▒》50%",
+    "《 ██████████▒▒》80%",
+    "《 ████████████》100%"
+  ];
+  
+  // Mensaje final
+  const finalMessage = `👋 Hola`;
+
+  // Enviar mensaje inicial
+  let { key } = await conn.sendMessage(m.chat, {text: `🤍 Calculando...`}, {quoted: m});
+  
+  // Animar las barras de carga
+  for (let i = 0; i < loadingBars.length; i++) {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    await conn.sendMessage(m.chat, {text: loadingBars[i], edit: key}, {quoted: m});
+  }
+
+  // Mensaje final
+  await conn.sendMessage(m.chat, {text: finalMessage, edit: key}, {quoted: m});
+};
+
+handler.help = ['hola'];
+handler.tags = ['fun'];
+handler.command = ['prueba2'];
+export default handler;
