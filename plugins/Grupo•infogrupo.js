27cc20523c321672320e05bb8d5dbf2a8cca0749
felix-proyects <frasesbebor@gmail.com>
2025-09-07 17:10:32 -0400

    Actualizar Grupo•infogrupo.js

--- plugins/Grupo•infogrupo.js
+++ plugins/Grupo•infogrupo.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, participants, groupMetadata}) => {
   const groupAdmins = participants.filter((p) => p.admin)
   const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
   const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
-  const text = `*「✿」Grupo ◢ ${groupMetadata.subject} ◤
+  const text = `*「✿」Grupo ◢ ${groupMetadata.subject} ◤*
  
 ❏ Usuarios: ${participants.length}
 ☆ *Creador:* @${owner.split('@')[0]}
