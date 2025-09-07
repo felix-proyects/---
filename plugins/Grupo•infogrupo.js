7bcd59e3cd163c828d6d3fa3ac10c7ede6254ef6
felix-proyects <frasesbebor@gmail.com>
2025-09-07 17:06:51 -0400

    Actualizar Grupo•infogrupo.js

--- plugins/Grupo•infogrupo.js
+++ plugins/Grupo•infogrupo.js
@@ -1,21 +1,22 @@
 const handler = async (m, {conn, participants, groupMetadata}) => {
-  const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || `${global.icons}`;
-  const {antiToxic, reaction, antiTraba, antidelete, antiviewonce, welcome, detect, antiLink, antiLink2, modohorny, autosticker, audios} = global.db.data.chats[m.chat];
-  const groupAdmins = participants.filter((p) => p.admin);
-  const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n');
-  const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
-  const text = `*「✿」Grupo ◢ ${groupMetadata.subject} ◤*
+  const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => global.icono);
+  const { antiLink, detect, welcome, modoadmin, autoRechazar, nsfw, autoAceptar, reaction, isBanned, antifake } = global.db.data.chats[m.chat]
+  const groupAdmins = participants.filter((p) => p.admin)
+  const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
+  const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
+  const text = `*「✿」Grupo ◢ ${groupMetadata.subject} ◤
+ 
+❏ Usuarios: ${participants.length}
+☆ *Creador:* @${owner.split('@')[0]}
+♡ Admins: ${listAdmin}
 
-❏ Usuarios » ${participants.length}
-☆ Registros » ${totalreg}
-♡ Creador » @${owner.split('@')[0]}
-♧ Bot principal » Undefined
+*Configuración del grupo*
 
-✐ ${botname} » *✓ Activado*
-✐ Anti-Links » *${antiLink ? ' ✓ Activado' : '✗ Desactivado'}*
-✐ Welcome » *${welcome ? ' ✓ Activado' : '✗ Desactivado'}*
-✐ Alertas » *${restrict ? ' ✓ Activado' : '✗ Desactivado'}* 
-`.trim();
+✐ *Bot* » ✓ Activado
+✐ *Welcome:* ${welcome ? '✓ Activado' : '✗ Desactivado'}
+✐ *Antilink:* ${antiLink ? '✓ Activado' : '✗ Desactivado'} 
+✐ *NSFW:* ${nsfw ? '✓ Activado' : '✗ Desactivado'}
+✐ *Modoadmin:* ${modoadmin ? '✓ Activado' : '✗ Desactivado'}`.trim();
   conn.sendFile(m.chat, pp, 'img.jpg', text, m, false, {mentions: [...groupAdmins.map((v) => v.id), owner]});
 };
 handler.help = ['infogrupo'];
@@ -23,4 +24,5 @@ handler.tags = ['grupo'];
 handler.command = ['infogrupo', 'gp'];
 handler.register = true
 handler.group = true;
+
 export default handler;
