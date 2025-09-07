8e7ca1dd242f84620450cecda8f0bc5a0e72149f
felix-proyects <frasesbebor@gmail.com>
2025-09-07 03:05:22 -0400

    Actualizar soporte-menu.js

--- plugins/soporte-menu.js
+++ plugins/soporte-menu.js
@@ -42,7 +42,7 @@ const handler = async (m, { conn }) => {
 • \`/soportelogs\` - Ver logs del sistema
 
 *╭─────────────────◆*
-*│  💫 Powered by Equipo de soporte*
+*│  💫 Powered by Deymoon ofc
 *│  🛡️ Panel de Soporte*
 *╰─────────────────◆*`;
 
