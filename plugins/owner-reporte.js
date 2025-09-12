94c541fc9bbaab7c17bc64409d551f7cf570e5a6
felix-proyects <frasesbebor@gmail.com>
2025-09-12 11:10:08 -0400

    Actualizar owner-reporte.js

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -1,10 +1,12 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) return conn.reply(m.chat, `${emoji} Por favor, ingrese el error que desea reportar.`, m)
-    if (text.length < 10) return conn.reply(m.chat, `${emoji} Especifique bien el error, mínimo 10 caracteres.`, m)
-    if (text.length > 1000) return conn.reply(m.chat, `${emoji2} *Máximo 1000 caracteres para enviar el error.`, m)
-    const teks = `〘 *R E P O R T E* 〙╭──────────
+    if (!text) return conn.reply(m.chat, `☆ Por favor, ingrese el error que desea reportar.`, m, fake)
+    if (text.length < 10) return conn.reply(m.chat, `☆ Especifique bien el error, mínimo 10 caracteres.`, m, fake)
+    if (text.length > 1000) return conn.reply(m.chat, `☆ *Máximo 1000 caracteres para enviar el error.`, m, fake)
+    const teks = `•  〘 *R E P O R T E* 〙  •
+
+╭──────────
 ├ *Bot:* 
-├⊷Sub-Bot
+├⊷ Sub-Bot
 ├ *Usuario:* 
 ├⊷${m.pushName || 'Anónimo'}
 ├ *Número:* 
@@ -14,7 +16,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 ╰──────────`
     await conn.reply(`50672110920@s.whatsapp.net`, m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
 
-    m.reply(`${emoji} El reporte se envío a mi creador, cualquier informe falso puede ocasionar baneo.`)
+    m.reply(`☆ El reporte esta en revisión, si esto es falso serás baneado.`)
 }
 handler.help = ['reportar']
 handler.tags = ['info']
