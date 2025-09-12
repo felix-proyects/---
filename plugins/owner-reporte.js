7b1cf131478ad39c2e96e84ff5dd98fcff5fce98
felix-proyects <frasesbebor@gmail.com>
2025-09-12 10:47:16 -0400

    Actualizar owner-reporte.js

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (text.length > 1000) throw '☆ *_Máximo 1000 caracteres para enviar el error._*'
     const teks = `〘 *R E P O R T E* 〙\n\n╭──────────\n├ Bot: Sub-Bot\n├Usuario: Wa.me/50672110920${m.sender.split`@`[0]}\n├Reporte: \n├ ${text}\n╰──────────`
     await conn.reply(global.owner[0][0] + '@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
-    m.reply('⚠️ *_El reporte se envío a mi creador, cualquier informe falso puede ocasionar baneo._*')
+    m.reply('☆ *_El reporte se envío a mi creador, cualquier informe falso puede ocasionar baneo._*')
 }
 handler.help = ['reportar']
 handler.tags = ['info']
