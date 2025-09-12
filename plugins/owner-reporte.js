c93617ec961dc7a8676bf8c26cd419ae0d11fda9
felix-proyects <frasesbebor@gmail.com>
2025-09-12 11:00:27 -0400

    Actualizar owner-reporte.js

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -1,13 +1,23 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw '⚠ *_️Ingrese el error ue desea reportar._*'
-    if (text.length < 10) throw '⚠️ *_Especifique bien el error, mínimo 10 caracteres._*'
-    if (text.length > 1000) throw '⚠️ *_Máximo 1000 caracteres para enviar el error._*'
-    const teks = `╭───────────────────\n│⊷〘 *R E P O R T E* 🤍 〙⊷\n├───────────────────\n│⁖🧡꙰  *Cliente:*\n│✏️ Wa.me/525544876071${m.sender.split`@`[0]}\n│\n│⁖💚꙰  *Mensaje:*\n│📩 ${text}\n╰───────────────────`
-    await conn.reply(global.owner[0][0] + '@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
-    m.reply('⚠️ *_El reporte se envío a mi creador, cualquier informe falso puede ocasionar baneo._*')
+    if (!text) return conn.reply(m.chat, `${emoji} Por favor, ingrese el error que desea reportar.`, m)
+    if (text.length < 10) return conn.reply(m.chat, `${emoji} Especifique bien el error, mínimo 10 caracteres.`, m)
+    if (text.length > 1000) return conn.reply(m.chat, `${emoji2} *Máximo 1000 caracteres para enviar el error.`, m)
+    const teks = `*✖️ \`R E P O R T E\` ✖️*
+
+☁️ Número:
+• Wa.me/${m.sender.split`@`[0]}
+
+👤 Usuario: 
+• ${m.pushName || 'Anónimo'}
+
+💬 Mensaje:
+• ${text}`
+    await conn.reply(`${suittag}@s.whatsapp.net`, m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
+
+    m.reply(`${emoji} El reporte se envío a mi creador, cualquier informe falso puede ocasionar baneo.`)
 }
 handler.help = ['reportar']
 handler.tags = ['info']
-handler.command = ['reporte','report','reportar','bug','error']
+handler.command = ['reporte', 'report', 'reportar', 'bug', 'error']
 
 export default handler
