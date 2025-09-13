d149ba6a889f9beced1bbcfc47f798c48d040ba0
felix-proyects <frasesbebor@gmail.com>
2025-09-13 18:02:21 -0400

    Actualizar Game•pokedex.js

--- plugins/Game•pokedex.js
+++ plugins/Game•pokedex.js
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, '🚩 *¿Que Pokémon quieres buscar?.*', m, rcanal)
+if (!text) return conn.reply(m.chat, '☆ *¿Que Pokémon quieres buscar?.*', m, fake)
 await m.react(rwait)
-conn.reply(m.chat, `🍟 *Buscando ${text}*`, m, {
+conn.reply(m.chat, `☆ *Buscando ${text}*`, m, fake, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: dev,
@@ -14,9 +14,9 @@ const response = await fetch(url);
 const json = await response.json();
 if (!response.ok) {
 await m.react(error)
-return conn.reply(m.chat, '🍟 *¡Oops! Parece que hubo un error al buscar el Pokémon. Por favor, inténtalo de nuevo más tarde.*', m, rcanal)}
-const aipokedex = `🚩 *Pokedex - Información de ${json.name}*\n\n🍟 *Nombre:* ${json.name}\n🍟 *ID:* ${json.id}\n🍟 *Tipo:* ${json.type}\n🍟 *Habilidades:* ${json.abilities}\n*Tamaño:* ${json.height}\n🍟 *Peso:* ${json.weight}\n\n📖 *Descripción:*\n${json.description}\n\n🔍 ¡Encuentra más detalles sobre este Pokémon en la Pokedex! 🔍\n\n🔗 https://www.pokemon.com/es/pokedex/${json.name.toLowerCase()}`
-conn.reply(m.chat, aipokedex, m, rcanal)
+return conn.reply(m.chat, '☆ *¡Oops! Parece que hubo un error al buscar el Pokémon. Por favor, inténtalo de nuevo más tarde.*', m, fake)}
+const aipokedex = `☆ *Pokedex - Información de ${json.name}*\n\n☆ *Nombre:* ${json.name}\n☆ *ID:* ${json.id}\n☆ *Tipo:* ${json.type}\n☆ *Habilidades:* ${json.abilities}\n*Tamaño:* ${json.height}\n☆ *Peso:* ${json.weight}\n\n📖 *Descripción:*\n${json.description}\n\n🔍 ¡Encuentra más detalles sobre este Pokémon en la Pokedex! 🔍\n\n🔗 https://www.pokemon.com/es/pokedex/${json.name.toLowerCase()}`
+conn.reply(m.chat, aipokedex, m, fake)
 await m.react(done) }
 
 handler.help = ['pokedex *<pokemon>*']
