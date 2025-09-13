7b133c6e5ebbc7956b81f967036b350b85e08ee3
felix-proyects <frasesbebor@gmail.com>
2025-09-13 13:38:35 -0400

    Actualizar gc-kick.js

--- plugins/gc-kick.js
+++ plugins/gc-kick.js
@@ -1,6 +1,6 @@
 var handler = async (m, { conn, participants, usedPrefix, command }) => {
     if (!m.mentionedJid[0] && !m.quoted) {
-        return conn.reply(m.chat, '> _Responde un mensaje o etiqueta a las persona que quieres que expulse del grupo._*', m, rcanal );
+        return conn.reply(m.chat, '> _Responde un mensaje o etiqueta a las persona que quieres que expulse del grupo._*', m, fake );
     }
 
     let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
@@ -11,20 +11,20 @@ var handler = async (m, { conn, participants, usedPrefix, command }) => {
     //const nn = conn.getName(m.sender);
 
     if (user === conn.user.jid) {
-        return conn.reply(m.chat, '☆ No puedo eliminar el bot del grupo', m, fake, );
+        return conn.reply(m.chat, 'Como crees que me voy a eliminar yo mismo, pendejo de mierda\n\nOtorino la ringologo o o o otorinolarigologo oso será león tu me la chupas y yo disfruto XDDDDDD', m, fake, );
     }
 
     if (user === ownerGroup) {
-        return conn.reply(m.chat, '☆ No puedo eliminar al propietario del grupo', m, fake, );
+        return conn.reply(m.chat, '☆ No puedo eliminar a este man del grupo', m, fake, );
     }
 
     if (user === ownerBot) {
-        return conn.reply(m.chat, '☆ No puedo eliminar al propietario del bot', m, fake );
+        return conn.reply(m.chat, '☆ No puedo eliminar a este crack', m, fake );
     }
 
     await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
 
-//conn.reply('18293142989@s.whatsapp.net', `☆ Un Admin Acabo De Eliminar Un Usuario En El Grupo:\n> ${groupMetadata.subject}.`, m, fake, );
+//conn.reply('18293478038@s.whatsapp.net', `☆ Un Admin Acabo De Eliminar Un Usuario En El Grupo:\n> ${groupMetadata.subject}.`, m, fake, );
 };
 
 handler.help = ['kick'];
