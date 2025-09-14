addc0801d03d62af4386306788a3c4aa35289c9f
felix-proyects <frasesbebor@gmail.com>
2025-09-14 03:08:48 -0400

    Actualizar Buscador-pornohub.js

--- plugins/Buscador-pornohub.js
+++ plugins/Buscador-pornohub.js
@@ -2,8 +2,17 @@ import fs from 'fs'
 import path from 'path'
 
 var handler = async (m, { conn, isPrems }) => {
-  let text = `「✿」El codigo va bien`
-  conn.reply(m.chat, text, m, fake)
+  let text = `「✿」El código va bien`
+  // Foto principal (puedes poner el link que quieras)
+  let mainPhoto = 'https://i.imgur.com/8fK4h6F.jpeg' // Foto grande
+  // Miniatura o cuadro pequeño (también puedes cambiar el link)
+  let thumbnailLink = 'https://i.imgur.com/5RjB2vF.jpeg' // Foto pequeña
+
+  await conn.sendMessage(m.chat, {
+    image: { url: mainPhoto },
+    caption: text,
+    jpegThumbnail: await (await fetch(thumbnailLink)).arrayBuffer(),
+  }, { quoted: m })
 }
 
 handler.help = ['prueba2']
