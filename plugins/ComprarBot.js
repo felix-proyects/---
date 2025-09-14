18051eb33f01a3cba955c1bc0ac35f5e70dbbb4f
felix-proyects <frasesbebor@gmail.com>
2025-09-14 01:11:15 -0400

    Actualizar ComprarBot.js

--- plugins/ComprarBot.js
+++ plugins/ComprarBot.js
@@ -5,11 +5,26 @@ handler.command ='comprarbot',/^(ComprarBot|Comprar|comprar|ComprarBot)$/i;
 export default handler;
 
 global.ComprarBot = `
-〔 *MAKIMA - BOT* 〕
+〔 *Bot premium* 〕
 
-*BOT PARA GRUPO* :
-> wa.me/18293142989
-
-*BOT PERZONALIZADO* :
-> wa.me/18293142989
+Gratis
 `;
+
+    const buttons = [
+        {
+      
+      buttonId: `#sockets`, 
+      buttonText: { 
+        displayText: "✿ Bots activos ✿" 
+      }, 
+      type: 1 
+    },
+    { 
+      buttonId: `#memev`,
+      buttonText: {
+        displayText: "✿ Ver más ✿"
+      },
+      type: 1
+    }
+
+    ];
