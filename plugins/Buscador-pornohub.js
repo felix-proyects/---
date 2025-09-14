f3245354127bbed24cbf31fb0d25577ca191ff42
felix-proyects <frasesbebor@gmail.com>
2025-09-14 03:27:57 -0400

    Actualizar Buscador-pornohub.js

--- plugins/Buscador-pornohub.js
+++ plugins/Buscador-pornohub.js
@@ -1,31 +1,65 @@
 import fs from 'fs'
 import path from 'path'
 
-var handler = async (m, { conn, isPrems }) => {
-  let text = `Botones Del Bot`
-  const buttons = [
+const menu = {
+  text: `「✿」Elige una opción`,
+  buttons: [
     {
-      buttonId: `#reg Deymoon.18`,
+      buttonId: `#opcion1`,
       buttonText: {
-        displayText: "✿ Auto Registrar ✿"
+        displayText: "Opción 1"
+      },
+      type: 1
+    },
+    {
+      buttonId: `#opcion2`,
+      buttonText: {
+        displayText: "Opción 2"
+      },
+      type: 1
+    },
+    {
+      buttonId: `#opcion3`,
+      buttonText: {
+        displayText: "Opción 3"
       },
       type: 1
     }
-  ];
+  ]
+};
 
+const opciones = {
+  '#opcion1': async (m, { conn }) => {
+    await conn.reply(m.chat, 'Has elegido la opción 1')
+  },
+  '#opcion2': async (m, { conn }) => {
+    await conn.reply(m.chat, 'Has elegido la opción 2')
+  },
+  '#opcion3': async (m, { conn }) => {
+    await conn.reply(m.chat, 'Has elegido la opción 3')
+  }
+};
+
+var handler = async (m, { conn, isPrems }) => {
   await conn.sendMessage(
     m.chat,
     {
-      text,
-      buttons,
+      text: menu.text,
+      buttons: menu.buttons,
       viewOnce: true
     },
     { quoted: m }
   );
 };
 
-handler.help = ['boton <opción>']
+handler.help = ['menu']
 handler.tags = ['main']
-handler.command = ['buttons', 'botones']
+handler.command = ['boton']
+
+handler.before = async (m, { conn }) => {
+  if (opciones[m.text]) {
+    await opciones[m.text](m, { conn })
+  }
+}
 
 export default handler
