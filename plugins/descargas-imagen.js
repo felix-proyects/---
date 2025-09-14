dcfd40b5b9f43d7266d5f3f2adedaf7f61a6efb8
felix-proyects <frasesbebor@gmail.com>
2025-09-14 03:37:34 -0400

    Actualizar descargas-imagen.js

--- plugins/descargas-imagen.js
+++ plugins/descargas-imagen.js
@@ -1,14 +1,14 @@
 import { googleImage } from '@bochilteam/scraper';
 
 const handler = async (m, { conn }) => {
-  const text = m.text.split(' ')[1];
-  if (!text) throw `*✿ Uso Correcto: imagen <query>*`;
+  const text = m.text.split(' ').slice(1).join(' ');
+  if (!text) {
+    conn.reply(m.chat, '✿ Debes ingresar un texto para buscar las imágenes', m, fake);
+    return;
+  }
 
   conn.reply(m.chat, '✿ *Buscando su imagen...*', m, fake);
   const res = await googleImage(text);
-  const image = await res.getRandom();
-  const link = image;
-
   const messages = [
     ['Imagen 1', 'Deymoon Club', await res.getRandom(), [], [], [], []],
     ['Imagen 2', 'Deymoon Club', await res.getRandom(), [], [], [], []],
