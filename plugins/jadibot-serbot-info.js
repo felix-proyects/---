93794a550a5a3b1311801dea7d4c5e27135ea3aa
felix-proyects <frasesbebor@gmail.com>
2025-09-14 10:02:22 -0400

    Actualizar jadibot-serbot-info.js

--- plugins/jadibot-serbot-info.js
+++ plugins/jadibot-serbot-info.js
@@ -11,11 +11,11 @@ async function handler(m, { conn: stars, usedPrefix }) {
 
   let users = [...uniqueUsers.values()]
 
-  let message = users.map((v, index) => `*#${index + 1} »* ${v.user.name || '-'}\n  ↳ wa.me/${v.user.jid.replace(/[^0-9]/g, '')}\n> 🜸 Tipo » (𝐒𝐮𝐛-𝐁𝐨𝐭)`).join('\n\n')
+  let message = users.map((v, index) => `*#${index + 1} »* ${v.user.name || '-'}\n*❏* wa.me/${v.user.jid.replace(/[^0-9]/g, '')}\n> 🜸 Tipo » (𝐒𝐮𝐛-𝐁𝐨𝐭)`).join('\n\n')
 
   let replyMessage = message.length === 0 ? '' : message
   let totalUsers = users.length
-  let responseMessage = `ꕥ LISTA DE BOTS ACTIVOS*\n\n❀ Principal » *1*\n☆ Premium » *1*\n*✿ Sub-Bots »* ${totalUsers || '0'}\n\nNúmeros de los Bots\n\n${replyMessage.trim()}`.trim()
+  let responseMessage = `*ꕥ LISTA DE BOTS ACTIVOS*\n\n❀ Principal » *1*\n☆ Premium » *1*\n*✿ Sub-Bots »* ${totalUsers || '0'}\n\nNúmeros de los Bots\n\n${replyMessage.trim()}`.trim()
 
   await stars.sendMessage(m.chat, { text: responseMessage, ...fake }, { quoted: m })
 }
