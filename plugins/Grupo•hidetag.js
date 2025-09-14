e62ff4485f09f2bc3b2859f5aaba461cd9c48568
felix-proyects <frasesbebor@gmail.com>
2025-09-14 05:38:30 -0400

    Actualizar Grupo•hidetag.js

--- plugins/Grupo•hidetag.js
+++ plugins/Grupo•hidetag.js
@@ -2,30 +2,21 @@ import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
 import * as fs from 'fs'
 
 var handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
-
-if (!m.quoted && !text) return conn.reply(m.chat, `☆ Ingresa un texto para hacer el tag`, m, fake)
-
+if (!m.quoted && !text) return conn.reply(m.chat, `✿ Debes enviar un texto para hacer un tag.`, m, fake)
 try { 
-
 let users = participants.map(u => conn.decodeJid(u.id))
 let q = m.quoted ? m.quoted : m || m.text || m.sender
 let c = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender
 let msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, { [m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : { text: '' || c }}, { quoted: null, userJid: conn.user.id }), text || q.text, conn.user.jid, { mentions: users })
 await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
-
-} catch {  
-
-/**
-[ By @NeKosmic || https://github.com/NeKosmic/ ]
-**/  
-
+} catch {    
 let users = participants.map(u => conn.decodeJid(u.id))
 let quoted = m.quoted ? m.quoted : m
 let mime = (quoted.msg || quoted).mimetype || ''
 let isMedia = /image|video|sticker|audio/.test(mime)
 let more = String.fromCharCode(8206)
 let masss = more.repeat(850)
-let htextos = `${text ? text : "*Hola!!*"}`
+let htextos = `${text ? text : ''}`
 if ((isMedia && quoted.mtype === 'imageMessage') && htextos) {
 var mediax = await quoted.download?.()
 conn.sendMessage(m.chat, { image: mediax, mentions: users, caption: htextos, mentions: users }, { quoted: null })
@@ -39,16 +30,12 @@ conn.sendMessage(m.chat, { audio: mediax, mentions: users, mimetype: 'audio/mp4'
 var mediax = await quoted.download?.()
 conn.sendMessage(m.chat, {sticker: mediax, mentions: users}, { quoted: null })
 } else {
-await conn.relayMessage(m.chat, {extendedTextMessage:{text: `${masss}\n${htextos}\n`, ...{ contextInfo: { mentionedJid: users, externalAdReply: { thumbnail: icons, sourceUrl: redes }}}}}, {})
-}}
+await conn.reply(m.chat, htextos, null, { mentions: [users] })
+}}}
 
-}
-handler.help = ['hidetag']
-handler.tags = ['grupo']
-handler.command = ['hidetag', 'tag', 'notify']
-
-handler.group = true
+handler.help = ['hidetag', 'tag']
+handler.tags = ['group']
+handler.command = ['hidetag', 'tag']
 handler.admin = true
-handler.register = true
 
 export default handler
