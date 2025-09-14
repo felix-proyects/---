2cf7073006f9c1690624f193c8e3560c0b3523a2
felix-proyects <frasesbebor@gmail.com>
2025-09-14 00:37:31 -0400

    Actualizar Game•scared.js

--- plugins/Game•scared.js
+++ plugins/Game•scared.js
@@ -8,7 +8,7 @@ let senderName = conn.getName(senderId)
 let tiempo = 5 * 60
 if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempo * 1000) {
 let tiempo2 = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempo * 1000 - Date.now()) / 1000))
-m.reply(`☆ Ya has cometido un Crimen recientemente, espera ⏱️ *${tiempo2}* para cometer tu próximo Crimen y evitar ser atrapado.`)
+m.reply(`✿ Espera *${tiempo2}* para usar *#crime* de nuevo.`)
 return
 }
 cooldowns[m.sender] = Date.now()
@@ -26,7 +26,7 @@ case 0:
 users[senderId].chocolates += amountTaken
 users[randomUserId].chocolates -= amountTaken
 conn.sendMessage(m.chat, {
-text: `☆¡Lograste cometer tu crimen con exito!, acabas de robar *${amountTaken} Diamantes* a @${randomUserId.split("@")[0]}\n\nSe suman *+${amountTaken} Diamantes* a ${senderName}.`,
+text: `✿ Robaste *${amountTaken} Coins* a @${randomUserId.split("@")[0]}.`,
 contextInfo: { 
 mentionedJid: [randomUserId],
 }}, { quoted: m })
@@ -34,15 +34,14 @@ break
 case 1:
 let amountSubtracted = Math.min(Math.floor(Math.random() * (senderChocolates - minAmount + 1)) + minAmount, maxAmount)
 users[senderId].chocolates -= amountSubtracted
-conn.reply(m.chat, `☆ No fuiste cuidadoso y te atraparon mientras cometias tu crimen, se restaron *-${amountSubtracted} Diamantes* a ${senderName}.`, m, fake)
+conn.reply(m.chat, `✿ No tuviste cuidado y a ti ${senderName} te quitaron *-${amountSubtracted} Coins.*`, m, fake)
 break
 case 2:
 let smallAmountTaken = Math.min(Math.floor(Math.random() * (randomUserChocolates / 2 - minAmount + 1)) + minAmount, maxAmount)
 users[senderId].chocolates += smallAmountTaken
 users[randomUserId].chocolates -= smallAmountTaken
 conn.sendMessage(m.chat, {
-text: `☆ Lograste cometer tu crimen con exito, pero te descubrieron y solo lograste tomar *${smallAmountTaken} Diamantes* de @${randomUserId.split("@")[0]}\n\nSe suman *+${smallAmountTaken} Diamantes* a ${senderName}.`,
-contextInfo: { 
+text: `✿ Te encontraron robando y solo pudiste tomar *${smallAmountTaken} Coins* de @${randomUserId.split("@")[0]}.`, m, fake)
 mentionedJid: [randomUserId],
 }}, { quoted: m })
 break
