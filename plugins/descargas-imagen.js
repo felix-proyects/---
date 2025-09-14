d43e6fec91693ea386f10593c607aecf2c9a8a28
felix-proyects <frasesbebor@gmail.com>
2025-09-14 03:34:19 -0400

    Actualizar descargas-imagen.js

--- plugins/descargas-imagen.js
+++ plugins/descargas-imagen.js
@@ -1,21 +1,26 @@
-import {googleImage} from '@bochilteam/scraper';
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (!text) throw `*✿ Uso Correcto: ${usedPrefix + command} Deymoon ofc*`;
-conn.reply(m.chat, '✿ *Buscando su imagen...*', m, fake, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: wm,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
-const res = await googleImage(text);
-const image = await res.getRandom();
-const link = image;
-const messages = [['Imagen 1', dev, await res.getRandom(),
-[[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res.getRandom(), [[]], [[]], [[]], [[]]]]
-await conn.sendCarousel(m.chat, `✿ Resultados de: ${text}`, 'Deymoon Club', null, messages, m);
+import { googleImage } from '@bochilteam/scraper';
+
+const handler = async (m, { conn }) => {
+  const text = m.text.split(' ')[1];
+  if (!text) throw `*✿ Uso Correcto: imagen <query>*`;
+
+  conn.reply(m.chat, '✿ *Buscando su imagen...*', m, fake);
+  const res = await googleImage(text);
+  const image = await res.getRandom();
+  const link = image;
+
+  const messages = [
+    ['Imagen 1', 'Deymoon Club', await res.getRandom(), [], [], [], []],
+    ['Imagen 2', 'Deymoon Club', await res.getRandom(), [], [], [], []],
+    ['Imagen 3', 'Deymoon Club', await res.getRandom(), [], [], [], []],
+    ['Imagen 4', 'Deymoon Club', await res.getRandom(), [], [], [], []]
+  ];
+
+  await conn.sendCarousel(m.chat, `✿ Resultados de: ${text}`, 'Deymoon Club', null, messages, m);
 };
+
 handler.help = ['imagen <query>'];
-handler.tags = ['buscador','descargas'];
-handler.command = ['image','imagen'];
-handler.group = true;
+handler.tags = ['buscador', 'descargas'];
+handler.command = ['image', 'imagen'];
+
 export default handler;
