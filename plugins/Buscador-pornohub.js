695c4ef20aa6ef0b14bac8cfe7ce2d3d20091778
felix-proyects <frasesbebor@gmail.com>
2025-09-14 03:03:08 -0400

    Actualizar Buscador-pornohub.js

--- plugins/Buscador-pornohub.js
+++ plugins/Buscador-pornohub.js
@@ -2,10 +2,23 @@ import fs from 'fs'
 import path from 'path'
 
 var handler = async (m, { conn, isPrems }) => {
-  let text = `「✿」Exito
+  let text = `「✿」El codigo va bien`
+  let image = fs.readFileSync(path.join(__dirname, 'ruta/a/la/imagen.jpg')) // Reemplaza con la ruta de tu imagen
 
-Jajaja`
-  conn.reply(m.chat, text, m, fake)
+  conn.sendMessage(m.chat, {
+    image: image,
+    caption: text,
+    contextInfo: {
+      mentionedJid: [m.sender],
+      forwardingScore: 999,
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: 'tuNewsletterJid', // Reemplaza con el JID de tu newsletter
+        newsletterName: 'tuNombreDeNewsletter', // Reemplaza con el nombre de tu newsletter
+        serverMessageId: ''
+      }
+    }
+  }, { quoted: m })
 }
 
 handler.help = ['prueba2']
