6880f091ffb1522cc36b2647cf5b05a76a454dfb
felix-proyects <frasesbebor@gmail.com>
2025-09-14 14:00:37 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -31,9 +31,9 @@ global.sessions = 'Session'
 global.jadi = 'JadiBots'
 global.makiJadibts = true
 
-global.packsticker = `=͟͟͞𝐃𝐞𝐲𝐦𝐨𝐨𝐧 Club⏤͟͟͞͞★\n↳ deymoon-club.vercel.app\n\n🤖 Info:\n`
+global.packsticker = `⊹ 🐬 Deymoon🧠 Club\n↳ Deymoon.club/\n\n👹 Info:\n deymoon-club.vercel.app/`
 global.packname = `Deymoon`
-global.author = `👑Bot:\n↳ Tanjiro Kamado\n👹 Usuario:\n↳ @Desconocido`;
+global.author = `⊹ 👑Bot:\n⊹ ↳ Tanjiro Kamado\n👑 Usuario:\n⊹ ↳ @Desconocido`;
 global.wm = '⍴᥆ᥕᥱrᥱძ ᑲᥡ ძᥱᥡm᥆᥆ᥒ ᥆𝖿ᥴ';
 global.titulowm = '⏤͟͞ू⃪𝐃𝐞𝐲𝐦𝐨𝐨𝐧 𝐔𝐥𝐭𝐫𝐚 𝐁𝐨𝐭𑁯ᰍ';
 global.igfg = 'Deymoon Club'
