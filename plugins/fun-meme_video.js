07e1d63b2328f16192a8ac9b6836c5a195a3f237
felix-proyects <frasesbebor@gmail.com>
2025-09-14 00:52:53 -0400

    Actualizar fun-meme_video.js

--- plugins/fun-meme_video.js
+++ plugins/fun-meme_video.js
@@ -1,34 +1,45 @@
 let handler = async (m, { conn, usedPrefix }) => {
-    const memes = [
-        'https://qu.ax/DksQt.mp4',
-        'https://qu.ax/vDgQd.mp4',
-        'https://qu.ax/jpiBe.mp4',
-        'https://qu.ax/TiecS.mp4',
-        'https://qu.ax/LWJCF.mp4',
-        'https://qu.ax/euTXj.mp4',
-        'https://qu.ax/GoOJh.mp4',
-    ];
-
-    const randomMeme = memes[Math.floor(Math.random() * memes.length)];
-
-    const buttons = [
-        {
-            buttonId: `#code`,
-            buttonText: { displayText: "Ser Sub-Bot" },
-            type: 1
-        }
-    ];
-
-    await conn.sendMessage(
-        m.chat,
-        {
-            video: { url: randomMeme },
-            caption: "¡Aquí tienes un meme para disfrutar!",
-            buttons: buttons,
-            viewOnce: true
-        },
-        { quoted: m }
-    );
+  const memes = [                   
+    'https://qu.ax/vDgQd.mp4',
+    'https://qu.ax/jpiBe.mp4',
+    'https:                   
+    '//qu.ax/TiecS.mp4',
+    'https://qu.ax/LWJCF.mp4',
+    'https:                   
+    '//qu.ax/euTXj.mp4',
+    'https://qu.ax/GoOJh.mp4',
+  ];
+  const randomMeme = memes[Math.floor(Math.random() * memes.length)];
+  const buttons = [
+    { 
+      buttonId: `#code`, 
+      buttonText: { 
+        displayText: "Ser Sub-Bot" 
+      }, 
+      type: 1 
+    },
+    { 
+      buttonId: `#bots`, 
+      buttonText: { 
+        displayText: "Bots activos" 
+      }, 
+      type: 1 
+    }
+  ];
+  await conn.sendMessage(
+    m.chat, 
+    { 
+      video: { 
+        url: randomMeme 
+      }, 
+      caption: "¡Aquí tienes un meme para disfrutar!", 
+      buttons: buttons, 
+      viewOnce: true 
+    }, 
+    { 
+      quoted: m 
+    } 
+  );
 };
 
 handler.help = ['memev'];
