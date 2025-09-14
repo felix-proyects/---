fc181b89ac9246aaca74d54c38e365a049fc3298
felix-proyects <frasesbebor@gmail.com>
2025-09-14 01:28:28 -0400

    Actualizar Buscador-pornohub.js

---
+++ plugins/Buscador-pornohub.js
@@ -0,0 +1,11 @@
+const handler = async (m, {conn}) => {
+  m.reply(const.message);
+};
+
+const.message = 'Bot Premium', m, fake);
+
+handler.help = 'comprarbot';
+handler.tags = 'main';
+handler.command = ['prueba2'];
+
+export default handler;
