a6e119249e0dfb6f28748aadd576631585d63866
felix-proyects <frasesbebor@gmail.com>
2025-09-15 15:29:00 -0400

    Actualizar fun-trio.js

--- plugins/fun-trio.js
+++ plugins/fun-trio.js
@@ -25,6 +25,5 @@ handler.help = ['formartrio @usuario1 @usuario2'];
 handler.tags = ['fun'];
 handler.command = ['formartrio']
 handler.group = true;
-handler.register = true;
 
 export default handler;
