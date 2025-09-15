a47340c69ec7c26172a3a54a60c8f547a0b5ffa9
felix-proyects <frasesbebor@gmail.com>
2025-09-15 00:45:13 -0400

    Actualizar Buscador•google.js

--- plugins/Buscador•google.js
+++ plugins/Buscador•google.js
@@ -1,27 +1,22 @@
-import {googleIt} from '@bochilteam/scraper'
-import google from 'google-it'
-import axios from 'axios'
-let handler = async (m, { conn, command, args, usedPrefix }) => {
-const fetch = (await import('node-fetch')).default;
-const text = args.join` `
-if (!text) return conn.reply(m.chat, '🩵 Ingresa lo que deseas buscar en Google.', m, fake)
-conn.reply(m.chat, `🩵 Buscando Su Información...`, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: dev,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
-const url = 'https://google.com/search?q=' + encodeURIComponent(text)
-google({'query': text}).then(res => {
-let teks = `🩵 *Resultado de* : ${text}\n\n`
-for (let g of res) {
-teks += `🩵 *Titulo ∙* ${g.title}\n🩵 *Info ∙* ${g.snippet}\n🔗 *Url ∙* ${g.link}\n\n`
+import fs from 'fs'
+import path from 'path'
+
+var handler = async (m, { conn, isPrems }) => {
+  let text = `Está es la prueba de la lógica de botones mia`
+
+  const buttons = [
+    {
+      buttonId: `bb`,
+      buttonText: { displayText: "bb" },
+      type: 1
+    }
+  ];
+
+  conn.sendMessage(m.chat, { text, buttons }, { quoted: m })
 }
-conn.reply(m.chat, teks, m, fake)
-})
-}
-handler.help = ['google <búsqueda>']
-handler.tags = ['buscador']
-handler.command = ['google']
-handler.register = true 
+
+handler.help = ['prueba2']
+handler.tags = ['main']
+handler.command = ['buttons']
+
 export default handler
