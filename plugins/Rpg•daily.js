6be6422d363fa15c12d247440e98ed2888f32e22
felix-proyects <frasesbebor@gmail.com>
2025-09-15 08:28:32 -0400

    Actualizar Rpg•daily.js

--- plugins/Rpg•daily.js
+++ plugins/Rpg•daily.js
@@ -1,61 +1,46 @@
-import fs from 'fs'
-import path from 'path'
+let handler = async (m, { conn }) => {
+  let user = global.db.data.users[m.sender];
 
-const free = 25
-const prem = 15
+  const tiempoMs = 86400000; 
+  const tiempoActual = Date.now();
+  const diferencia = tiempoActual - (user.lastclaim || 0);
 
-var handler = async (m, {conn, isPrems }) => {
-  // Obtener la moneda personalizada
-  const senderNumber = m.sender.replace(/[^0-9]/g, '')
-  const botPath = path.join('./JadiBots', senderNumber)
-  const configPath = path.join(botPath, 'config.json')
-  let moneda = 'Coins' // Valor por defecto
-  if (fs.existsSync(configPath)) {
-    try {
-      const config = JSON.parse(fs.readFileSync(configPath))
-      moneda = config.currency || 'Moneda'
-    } catch (e) {
-      // Si hay un error leyendo config, se queda el valor por defecto
-    }
+  if (diferencia < tiempoMs) {
+    let restante = msToTime((user.lastclaim + tiempoMs) - tiempoActual);
+    return conn.reply(m.chat, `Vuelve en *${restante}* para no perder tu fecha.`, m, fake);
   }
 
-  let coin = `${pickRandom([5, 6, 7, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 99, 100, 110, 120, 130, 600, 1000, 1500])}` * 1
-  let exp = `${pickRandom([500, 600, 700, 800, 900, 999, 1000, 1300, 1500, 1800])}` * 1
-  let exppremium = `${pickRandom([1000, 1500, 1800, 2100, 2500, 2900, 3300, 3600, 4000, 4500])}` * 1
-  let d = Math.floor(Math.random() * 30)
-  global.db.data.users[m.sender].chocolates += d
-  global.db.data.users[m.sender].money += d
-  let time = global.db.data.users[m.sender].lastclaim + 2 * 60 * 1000 // 2 minutos en milisegundos
-  if (new Date - global.db.data.users[m.sender].lastclaim < 2 * 60 * 1000) 
-    return conn.reply(m.chat, `*Vuelve en ${msToTime(time - new Date())}*`, m, fake)
-  global.db.data.users[m.sender].exp += isPrems ? exppremium : exp
+  if (diferencia > tiempoMs * 2) {
+    user.dailyStreak = 1;
+  } else {
+    user.dailyStreak = (user.dailyStreak || 0) + 1;
+  }
 
-  let str = `「✿」Has reclamado tu recompensa de cada 2 minutos de *+${isPrems ? exppremium : exp}* XP y +${coin} Coins
+  let streak = user.dailyStreak;
+  let reward = 30000 + (streak * 5000);
+  let diamantes = Math.floor(5 + streak);
+  let exp = Math.floor(200 + streak * 20);
 
-> Próxima recompensa: *${d}*`
-  conn.reply(m.chat, str, m, fake)
-  global.db.data.users[m.sender].lastclaim = new Date * 1
+  user.coin += reward;
+  user.diamond += diamantes;
+  user.exp += exp;
+  user.lastclaim = tiempoActual;
+
+  conn.reply(m.chat, `「✿」Has reclamado tu recompensa diaria de *¥${reward.toLocaleString()} ${moneda}*! (Día *${streak}*)\n` +
+    `> Día *${streak + 1}* » *+¥${(reward + 5000).toLocaleString()}*`, m, fake);
 }
 
-handler.help = ['daily', 'claim']
-handler.tags = ['rpg']
-handler.command = ['daily', 'claim']
+handler.help = ['daily', 'diario'];
+handler.tags = ['rpg'];
+handler.command = ['daily', 'diario'];
+handler.group = true;
+handler.register = true;
 
-export default handler
-
-function pickRandom(list) {
-  return list[Math.floor(Math.random() * list.length)]
-}
+export default handler;
 
 function msToTime(duration) {
-  var milliseconds = parseInt((duration % 1000) / 100),
-  seconds = Math.floor((duration / 1000) % 60),
-  minutes = Math.floor((duration / (1000 * 60)) % 60),
-  hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+  let hours = Math.floor(duration / (1000 * 60 * 60));
+  let minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
 
-  hours = (hours < 10) ? '0' + hours : hours
-  minutes = (minutes < 10) ? '0' + minutes : minutes
-  seconds = (seconds < 10) ? '0' + seconds : seconds
-
-  return minutes + ' Minutos ' + seconds + ' Segundos'
+  return `${hours} hora(s) y ${minutes} minuto(s)`;
 }
